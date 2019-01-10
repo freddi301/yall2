1fcbc80ccc83095563ebc25e07f2424df7ab16c4
Frederik Batuna <gobi301@gmail.com>
2019-01-10 20:19:11 +0100

    add: dist.exec. firebase store implementation

--- ide/src/core/distributedExecution.ts
+++ ide/src/core/distributedExecution.ts
@@ -10,11 +10,13 @@ import {
 } from "../language/Yall.External";
 import { End } from "../language/Yall.Pauseable";
 import { fromPurescriptAst } from "./fromPurescriptAst";
+import * as firebase from "firebase/app";
+import "firebase/database";
 
 // key value distributed store (eventual consistency)
 interface Store<Key extends Serializable, Value extends Serializable> {
-  read(key: Key): Promise<{ value: Value; dependencies: Key[] }>;
-  save(value: Value, dependencies: Key[]): Promise<Key>;
+  read(key: Key): Promise<{ value: Value }>;
+  save(value: Value): Promise<Key>;
   incrementReferenceCount(key: Key): Promise<void>;
   decrementReferenceCount(key: Key): Promise<void>;
 }
@@ -42,7 +44,7 @@ type Serializable = any;
 class LocalInMemoryStore implements Store<string, BasicAst> {
   private data = {} as Record<
     string,
-    { value: BasicAst; dependencies: string[]; referenceCount: number }
+    { value: BasicAst; referenceCount: number }
   >;
   private nextKey = 0;
   public async read(key: string) {
@@ -52,9 +54,9 @@ class LocalInMemoryStore implements Store<string, BasicAst> {
     }
     throw new Error("not found");
   }
-  public async save(value: BasicAst, dependencies: string[]) {
+  public async save(value: BasicAst) {
     const key = String(this.nextKey++);
-    this.data[key] = { value, dependencies, referenceCount: 1 };
+    this.data[key] = { value, referenceCount: 1 };
     return key;
   }
   public async incrementReferenceCount(key: string) {
@@ -82,25 +84,20 @@ class LocalSingleThreadedCluster<Key extends Serializable>
     }
     if (step instanceof End) {
       const resultUri = await this.store.save(
-        fromPurescriptAst(getResult(step)),
-        term.dependencies
+        fromPurescriptAst(getResult(step))
       );
       return { type: "result", term: resultUri };
     }
     const intermediate = getResult(step);
     const intermediateUri = await this.store.save(
-      fromPurescriptAst(intermediate),
-      term.dependencies
+      fromPurescriptAst(intermediate)
     );
     const placeholder = this.makePlaceholder(intermediateUri);
     step = nextWith(placeholder)(step);
     while (!(step instanceof End)) {
       step = nextNoRecur(step);
     }
-    const resultUri = await this.store.save(
-      fromPurescriptAst(getResult(step)),
-      term.dependencies.concat([intermediateUri])
-    );
+    const resultUri = await this.store.save(fromPurescriptAst(getResult(step)));
     return { type: "fork", parent: resultUri, child: intermediateUri };
   }
   public async run(termKey: Key): Promise<Key> {
@@ -127,8 +124,7 @@ class LocalSingleThreadedCluster<Key extends Serializable>
           toPurescriptAst({ ast: child.value, path: [] })
         )(toPurescriptAst({ ast: parent.value, path: [] }));
         const continuationPointer = await this.store.save(
-          fromPurescriptAst(continuation),
-          [...parent.dependencies, ...child.dependencies]
+          fromPurescriptAst(continuation)
         );
         await this.store.decrementReferenceCount(parentKey);
         await this.store.decrementReferenceCount(childKey);
@@ -154,7 +150,7 @@ function LocalClient<Key extends Serializable>({
   return {
     async run(ast: BasicAst) {
       const source = ast;
-      const sourcePointer = await store.save(source, []);
+      const sourcePointer = await store.save(source);
       const resultPointer = await cluster.run(sourcePointer);
       const { value } = await store.read(resultPointer);
       await store.decrementReferenceCount(sourcePointer);
@@ -164,6 +160,53 @@ function LocalClient<Key extends Serializable>({
   };
 }
 
-const store = new LocalInMemoryStore();
+class FirebaseStore implements Store<string, BasicAst> {
+  private app = firebase.initializeApp({
+    apiKey: "AIzaSyA-_VwJwwTIYRP3h9wReqTZjmRRhD7pP0o",
+    authDomain: "distributed-lambda-calculus.firebaseapp.com",
+    databaseURL: "https://distributed-lambda-calculus.firebaseio.com",
+    projectId: "distributed-lambda-calculus",
+    storageBucket: "",
+    messagingSenderId: "900059068998"
+  });
+  private data = this.app.database().ref("data");
+  public async read(key: string) {
+    const result = (await this.data.child(key).once("value")).val();
+    return result as any;
+  }
+  public async save(value: BasicAst) {
+    const ref = this.data.push();
+    await ref.set({ value, referenceCount: 1 });
+    return ref.key as string;
+  }
+  public async incrementReferenceCount(key: string) {
+    await this.updateReference(key, n => n + 1);
+  }
+  public async decrementReferenceCount(key: string) {
+    const newRefCount = await this.updateReference(key, n => n - 1);
+    if (newRefCount <= 0) {
+      await this.data.child(key).remove();
+    }
+  }
+  private async updateReference(
+    key: string,
+    f: (refCount: number) => number
+  ): Promise<number> {
+    const actual: number = (await this.data
+      .child("key")
+      .child("referenceCount")
+      .once("value")).val() as any;
+    const newCount = f(actual);
+    await this.data
+      .child(key)
+      .child("referenceCount")
+      .set(newCount);
+    return newCount;
+  }
+}
+
+export const localStore = new LocalInMemoryStore();
+export const firebaseStore = new FirebaseStore();
+const store = firebaseStore;
 const cluster = new LocalSingleThreadedCluster(store);
 export const distributedExecutionDemo = LocalClient({ store, cluster });
